d7c280c6054b55a852812d83f18117a6af353d77
carinalsilverio <carinalsilverio@gmail.com>
2022-08-22 22:13:35 -0300

    Ajustes cards mobile e animações geral

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// var glide = new Glide('#intro', {
+//   type: 'carousel',
+//   perView: 4,
+//   focusAt: 'center',
+//   breakpoints: {
+//     800: {
+//       perView: 2,
+//     },
+//     480: {
+//       perView: 1,
+//     },
+//   },
+// });
+
+// glide;
+
+// var exibirP = document.querySelector(".btn")
+
+// exibirP.addEventListener('click', function(){
+//   if(exibirP.style.display === 'block') {
+//     exibirP.style.display === 'none'
+//   }else {
+//     conhecimentosP.style.display === 'block'
+//   }
+// })
