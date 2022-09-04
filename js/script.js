b3dd90dda7a1482aa3a698e64d3b4cc9e4cb1b10
carinalsilverio <carinalsilverio@gmail.com>
2022-09-03 22:12:39 -0300

    Menu mobile, animação barra de rolagem

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+//Animação Scroll
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + '%';
+};
+
+//Carousel
+
 const controls = document.querySelectorAll('.control');
 let currentItem = 0;
 const items = document.querySelectorAll('.item');
@@ -20,7 +31,7 @@ controls.forEach((control) => {
     if (currentItem < 0) {
       currentItem = maxItems - 1;
     }
-    console.log("control", isLeft, currentItem);
+    console.log('control', isLeft, currentItem);
 
     items.forEach((item) => item.classList.remove('current-item'));
 
@@ -32,3 +43,22 @@ controls.forEach((control) => {
     items[currentItem].classList.add('current-item');
   });
 });
+
+//Menu mobile
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
